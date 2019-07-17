52382905d84d2bf9b3ee99d0549b7c0d84dee3eb
Matthew <matthew.yu007@gmail.com>
2019-07-17 13:52:45 -0700

    added listenAndStoreClients, storeMessages, sendMessages functions. Added threading capability

--- client2.py
+++ client2.py
@@ -1,2 +1,6 @@
 import socket
 import threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 4000))
+
--- server.py
+++ server.py
@@ -1,2 +1,39 @@
 import socket
 import threading
+
+clientList = []
+newThreads = []
+allActiveThreads = []
+messages = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 4000))
+print("Binded socket")
+s.listen(5)
+
+def listenAndStoreClients(): 
+    while True: 
+        print("listening")
+        c, a = s.accept()
+        clientList.append((c,a))
+        print(f"{a} has connected")
+    
+def storeMessages(): 
+    while True: 
+        print("storing")
+
+def sendMessages(): 
+    while True: 
+        for x in messages: 
+            print("x")
+
+
+newThreads.append(threading.Thread(target=listenAndStoreClients)) #stores thread that listens and stores client in newThreads
+newThreads.append(threading.Thread(target=storeMessages))
+newThreads.append(threading.Thread(target=sendMessages))
+
+while True: 
+    for x in newThreads: #starts any inactive threads stored in newThreads, moves active threads into allActiveThreads
+        x.start()
+        allActiveThreads.append(x)
+        newThreads.pop(newThreads.index(x))
