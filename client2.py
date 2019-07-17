7aeb48c26dbede11c6cea60c65714a5c7bf34a2c
Matthew <matthew.yu007@gmail.com>
2019-07-17 16:44:58 -0700

    nearly complete. Need to add filter when displaying messages

--- client2.py
+++ client2.py
@@ -1,6 +1,28 @@
 import socket
 import threading
 
+newThreads = []
+allActiveThreads = []
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 4000))
+s.connect((socket.gethostname(), 40000))
 print("Connected to server")
+
+
+def sendInput(): 
+    while True: 
+        s.send(bytes(input(), "UTF-8"))
+
+
+def recieveAndPrint(): 
+    while True: 
+        print(s.recv(2048).decode("UTF-8"))
+
+newThreads.append(threading.Thread(target=sendInput))
+newThreads.append(threading.Thread(target=recieveAndPrint))
+
+
+while True: 
+    for x in newThreads: 
+        x.start()
+        allActiveThreads.append(x)
+        newThreads.pop(newThreads.index(x))
--- server.py
+++ server.py
@@ -7,16 +7,15 @@ allActiveThreads = []
 messages = []
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 4000))
-print("Binded socket")
+s.bind((socket.gethostname(), 40000))
+print("Server active")
 s.listen(5)
 
 def listenAndStoreClients(): #listens for new clients, stores new clients in a list called clientList
     while True: 
-        print("listening")
         c, a = s.accept()
         clientList.append((c,a))
-        print(f"{a} has connected")
+        print(f"{a[1]} has connected")
         newThreads.append(threading.Thread(target=recieveFromThisClient, args=(c,a,))) #creates new thread for this specific client
     
 
@@ -27,12 +26,15 @@ def recieveFromThisClient(c,a): #recieves and stores incomming messages to the m
         if incomingMessage != "": 
             fullMessage = str(a[1]) + " " + incomingMessage
             messages.append(fullMessage)
+        
 
 
 def sendMessages(): 
     while True: 
         for x in messages: 
-            print(x)
+            for y in clientList: 
+                y[0].send(bytes(x, "UTF-8"))
+            messages.pop(messages.index(x))
 
 
 newThreads.append(threading.Thread(target=listenAndStoreClients)) #stores thread that listens and stores client in newThreads
@@ -43,4 +45,5 @@ while True:
         x.start()
         allActiveThreads.append(x)
         newThreads.pop(newThreads.index(x))
-    print(messages)
+    
+    #print(messages)
