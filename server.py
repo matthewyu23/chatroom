b28d66f4b5d3fa7b5ba795b6a4c4c40526cf36be
Matthew <matthew.yu007@gmail.com>
2019-07-19 11:45:14 -0700

    adds documentation to server.py

--- server.py
+++ server.py
@@ -1,50 +1,47 @@
 import socket
 import threading
 
-clientList = []
-newThreads = []
-allActiveThreads = []
-messages = []
+clientList = [] #List that stores all clients (active and inactive)
+newThreads = [] #List that stores new (inactive) threads
+allActiveThreads = [] #List that stores active threads
+messages = [] #List that stores unsent messages (message will be deleted after message is sent)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 40000))
-#s.bind((socket.gethostname(), 40000))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Creating a socket
+s.bind((socket.gethostname(), 40000)) #Binding the socket to host's IP and port 40000
 print("Server active")
-s.listen(10)
+s.listen(10) #Listens for 10 clients
 
-def listenAndStoreClients(): #listens for new clients, stores new clients in a list called clientList
+def listenAndStoreClients(): #Function that listens for new clients, stores new clients in a list called clientList
     while True: 
-        c, a = s.accept()
+        c, a = s.accept() #Accecpts the client. c is an object that represents the client, a is their address
         clientList.append((c,a))
         print(f"{a[1]} has connected")
-        newThreads.append(threading.Thread(target=recieveFromThisClient, args=(c,a,))) #creates new thread for this specific client
-        c.send(bytes("ID:" + str(len(clientList)),"UTF-8"))
+        newThreads.append(threading.Thread(target=recieveFromThisClient, args=(c,a,))) #creates new thread that listens to this specific client
+        c.send(bytes("ID:" + str(len(clientList)),"UTF-8")) #Sends the client its ID (index on the client list)
     
 
 
-def recieveFromThisClient(c,a): #recieves and stores incomming messages to the messages list
+def recieveFromThisClient(c,a): #Function that recieves and stores incomming messages to the messages list
     while True: 
-        incomingMessage = c.recv(2048).decode("utf-8")
+        incomingMessage = c.recv(2048).decode("utf-8") #Reieves the message in byte string and decodes it
         if incomingMessage != "": 
             messages.append(incomingMessage)
         
 
 
-def sendMessages(): 
+def sendMessages(): #Function that sends all messages to all clients
     while True: 
         for x in messages: 
             for y in clientList: 
                 y[0].send(bytes(x, "UTF-8"))
-            messages.pop(messages.index(x))
+            messages.pop(messages.index(x)) #Adds message to the messages list
 
 
-newThreads.append(threading.Thread(target=listenAndStoreClients)) #stores thread that listens and stores client in newThreads
-newThreads.append(threading.Thread(target=sendMessages))
+newThreads.append(threading.Thread(target=listenAndStoreClients)) #Stores thread that listens and stores client in newThreads
+newThreads.append(threading.Thread(target=sendMessages)) #Stores thread that sends messages to all clients
 
 while True: 
-    for x in newThreads: #starts any inactive threads stored in newThreads, moves active threads into allActiveThreads
+    for x in newThreads: #Starts any inactive threads stored in newThreads list, moves active threads into allActiveThreads list
         x.start()
         allActiveThreads.append(x)
         newThreads.pop(newThreads.index(x))
-    
-    #print(messages)
